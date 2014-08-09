6c8a1bf4326d3e47cf4004b7ce173482f67cf71b
Joe Fitzgerald <jfitzgerald@gopivotal.com>
2014-08-09 15:29:11 -0600

    Adding Interface

---
+++ logger.go
@@ -0,0 +1,19 @@
+package standardlog
+
+// Logger represents log.Logger functions from the standard library
+type Logger interface {
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+	Fatalln(v ...interface{})
+	Flags() int
+	Output(calldepth int, s string) error
+	Panic(v ...interface{})
+	Panicf(format string, v ...interface{})
+	Panicln(v ...interface{})
+	Prefix() string
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+	SetFlags(flag int)
+	SetPrefix(prefix string)
+}
